5b62081e267acdf6a73f4aaf50f02e14c3709d88
Stuart MacPherson <stuart.macpherson@purplewifi.com>
2016-02-20 11:45:52 +0000

    Added testConfig.php

---
+++ Tests/testConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SMACP\MachineTranslator\Tests;
+
+class testConfig
+{
+    /** @const string */
+    const MICROSOFT_KEY = '';
+    
+    /** @const string */
+    const MICROSOFT_SECRET = '';
+}
